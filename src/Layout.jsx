d8a349464aada6f3b6a8e329a5485a1cdf910a50
Lior Cohen <klilior@gmail.com>
2026-01-08 11:03:55 +0000

    File changes

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 import { Link, useLocation } from "react-router-dom";
 import { createPageUrl } from "@/utils";
 import {
