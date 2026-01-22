48ec5f317d59e839834f0af9d9d2d0af0fcab96b
Lior Cohen <klilior@gmail.com>
2026-01-22 11:04:34 +0000

    File changes

--- functions/runLinetSync.ts
+++ functions/runLinetSync.ts
@@ -1,4 +1,4 @@
-import { createClientFromRequest } from 'npm:@base44/sdk@0.8.4';
+import { createClientFromRequest } from 'npm:@base44/sdk@0.8.6';
 import { format, subDays, parseISO, addMonths, addDays } from 'npm:date-fns@2.30.0';
 
 const BASE_URL = "https://app.linet.org.il/api";
