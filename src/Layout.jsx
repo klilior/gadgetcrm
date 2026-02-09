333188f48f91136c03ec3a93fa9a33b41c1b3456
Lior Cohen <klilior@gmail.com>
2026-02-09 14:21:43 +0000

    File changes

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -252,8 +252,7 @@ function AppContent({ children, currentPageName }) {
     { title: "הגדרות נוכחות", url: createPageUrl("AttendanceSettings") },
     { title: "נתוני מכירות", url: createPageUrl("SalesDataAdmin") },
     { title: "סנכרון לינט", url: createPageUrl("SyncManagement") },
-    { title: "הגדרות כלליות", url: createPageUrl("Settings") },
-    { title: "ניטור מחירים Zap", url: createPageUrl("PriceMonitor") }
+    { title: "הגדרות כלליות", url: createPageUrl("Settings") }
   ] : [];
 
   const toggleSection = (section) => {
