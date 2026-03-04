999e65dfa492cbe88f564ab83a910bc77b8a6c05
Lior Cohen <klilior@gmail.com>
2026-03-04 20:53:25 +0000

    File changes

--- functions/syncWooCommerceOrders.ts
+++ functions/syncWooCommerceOrders.ts
@@ -168,9 +168,10 @@ Deno.serve(async (req) => {
                 );
                 const customerNote = wooOrder.customer_note || billingNoteMeta?.value || '';
 
+                const resolvedClientId = clientId || existingOrders[0]?.client_id || '';
                 const orderData = {
                     external_order_number: wooOrder.id.toString(),
-                    client_id: clientId || (existingOrders[0]?.client_id || null),
+                    client_id: resolvedClientId || undefined,
                     order_date: wooOrder.date_created,
                     status: wooOrder.status,
                     total: wooOrder.total,
