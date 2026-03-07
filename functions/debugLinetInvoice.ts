fe6ad5cedc2aa6039c01189bc16b9dc192f09da7
Lior Cohen <klilior@gmail.com>
2026-03-07 21:03:13 +0000

    File changes

--- functions/debugLinetInvoice.ts
+++ functions/debugLinetInvoice.ts
@@ -42,7 +42,7 @@ Deno.serve(async (req) => {
       offset: 0,
       query: {
         doctype: ["9", "3", "4"],
-        issue_date: "2026-03-01 to 2026-03-07",
+        issue_date: "2026-03-01 to 2026-03-08",
       },
     };
 
