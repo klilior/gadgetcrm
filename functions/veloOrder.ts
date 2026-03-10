4cabb4bde0cb5861d7b808fba826af6b7cd51995
Lior Cohen <klilior@gmail.com>
2026-03-10 16:55:03 +0000

    File changes

--- functions/veloOrder.ts
+++ functions/veloOrder.ts
@@ -187,9 +187,6 @@ Deno.serve(async (req) => {
             customerAddress: {
                 first_name: firstName,
                 last_name: lastName,
-                firstName: firstName,
-                lastName: lastName,
-                name: `${firstName} ${lastName}`.trim(),
                 street: street,
                 number: number,
                 line2: '',
