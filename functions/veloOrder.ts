c2cf23c4ffdb548b10b45691ee72e162e622f0a6
Lior Cohen <klilior@gmail.com>
2026-03-18 20:37:28 +0000

    File changes

--- functions/veloOrder.ts
+++ functions/veloOrder.ts
@@ -1,4 +1,4 @@
-import { createClientFromRequest } from 'npm:@base44/sdk@0.8.20';
+import { createClientFromRequest } from 'npm:@base44/sdk@0.8.21';
 
 // Enterprise HMAC: sha256(jwt + apiKey, secret=apiSecret)
 async function veloHmac(jwt, apiKey, apiSecret) {
