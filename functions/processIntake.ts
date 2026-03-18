04917532c97e766792eb0b6560ef5ce61cd0987d
Lior Cohen <klilior@gmail.com>
2026-03-18 20:39:16 +0000

    File changes

--- functions/processIntake.ts
+++ functions/processIntake.ts
@@ -1,4 +1,4 @@
-import { createClientFromRequest } from 'npm:@base44/sdk@0.8.6';
+import { createClientFromRequest } from 'npm:@base44/sdk@0.8.21';
 
 function isValidFile(intake) {
   const allowed = ['application/pdf', 'image/jpeg', 'image/jpg', 'image/png'];
